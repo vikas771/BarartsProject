024193cef1c0f8d7fc27993c7c5a142015a770fd
fullstackexpert301 <fullstackexpert301@gmail.com>
2024-01-06 18:50:40 +0530

    Checklist folder optimizetion with comments

--- src/app/checklist/page.js
+++ src/app/checklist/page.js
@@ -1,21 +1,25 @@
-import React from 'react'
+import React from 'react';
 import Style from './checklist.module.css';
 import common from '../common.module.css';
-import { Grid } from '@mui/material'
+import { Grid } from '@mui/material';
 import DescriptionIcon from '@mui/icons-material/Description';
 import LinearProgress from '@mui/material/LinearProgress';
 import SearchIcon from '@mui/icons-material/Search';
 
-
-const page = () => {
+// Functional component representing the checklist page
+const ChecklistPage = () => {
     return (
         <>
+            {/* Parent container for the checklist */}
             <div className={Style.CheckParent}>
 
+                {/* Header section with checklist title and search bar */}
                 <Grid container spacing={2} className='mt-1'>
+                    {/* Checklist title */}
                     <Grid item xs={6} className={common.HeadingText}>
                         <h4>Checklist</h4>
                     </Grid>
+                    {/* Search bar */}
                     <Grid item xs={6} className={common.SearchParent}>
                         <input
                             type="text"
@@ -26,23 +30,29 @@ const page = () => {
                     </Grid>
                 </Grid>
 
+                {/* First checklist item */}
                 <div className={Style.FirstParent}>
+                    {/* Grid container for checklist item details */}
                     <Grid container spacing={2} className={Style.MainChild}>
+                        {/* Icon and text section */}
                         <Grid item xs={10} className={Style.IconAndText}>
                             <DescriptionIcon className={Style.IconStyle} />
                             <h5>Opening Sidework for Bartenders</h5>
                         </Grid>
+                        {/* Progress details section */}
                         <Grid item xs={2}>
+                            {/* Progress details */}
                             <div className={Style.ProgressParent}>
                                 <p>03/06</p>
                                 <span> In Progress</span>
                             </div>
 
+                            {/* Linear progress bar */}
                             <div className={Style.myProgressBar}>
                                 <LinearProgress
                                     className={Style.Demoo}
                                     variant="determinate"
-                                    value="25"
+                                    value={25}
                                     sx={{ '& .MuiLinearProgress-bar': { backgroundColor: 'rgba(255,131,65,255)' } }}
                                 />
                             </div>
@@ -50,6 +60,7 @@ const page = () => {
                     </Grid>
                 </div>
 
+                {/* Second checklist item (similar structure as the first) */}
                 <div className={Style.FirstParent}>
                     <Grid container spacing={2} className={Style.MainChild}>
                         <Grid item xs={10} className={Style.IconAndText}>
@@ -66,7 +77,7 @@ const page = () => {
                                 <LinearProgress
                                     className={Style.Demoo}
                                     variant="determinate"
-                                    value="25"
+                                    value={25}
                                     sx={{ '& .MuiLinearProgress-bar': { backgroundColor: 'rgba(255,131,65,255)' } }}
                                 />
                             </div>
@@ -75,7 +86,7 @@ const page = () => {
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default page
+export default ChecklistPage;
